73e348c84c4ea645042f22d7ccda0e32681a0384
Emre Dundar <emredundar@Mac.fritz.box>
2026-09-20 22:58:58 +0200

    Erster Commit: TodoList Projekt

---
+++ TodoList.hs
@@ -0,0 +1,13 @@
+-- A simple Todo List manager in Haskell
+
+module TodoList where
+
+type Task = String
+
+type TodoList = [Task]
+
+initialTasks :: TodoList
+initialTasks = "Learn Haskell" : "Do school homework" : []
+
+addTask :: Task -> TodoList -> TodoList
+addTask newTask currentList = currentList ++ [newTask]
